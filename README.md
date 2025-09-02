45971586548323685bf17c4e9facd076d4fe32cf
EstebanEM <139818678+EstebanEM@users.noreply.github.com>
2025-09-01 20:29:31 -0500

    Ajustes dentro de la sección de Eventos y galerias

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Globerth
+# Proyecto Angular - Globerth
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.0.3.
 
--- src/app/modules/inicio/pages/home/home.ts
+++ src/app/modules/inicio/pages/home/home.ts
@@ -1,14 +1,63 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: false,
   templateUrl: './home.html',
-  styleUrl: './home.sass'
+  styleUrls: ['./home.sass']
 })
-export class Home {
+export class Home implements OnInit, OnDestroy {
+  slides = [
+    { src: 'https://liceogloberth.edu.co/images/galeria2/1.english%20club.jpg', alt: 'Evento 1' },
+    { src: 'https://liceogloberth.edu.co/images/galeria2/2.%20entreba%20boletines.jpg', alt: 'Evento 2' },
+    { src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbx5Hm2igE8PJ2ODKsA0hIdyIRa0pkN5irsQ&s', alt: 'Evento 3' },
+    { src: 'https://img.freepik.com/psd-gratis/fondo-celebracion-dia-san-valentin-3d_23-2150100004.jpg?semt=ais_hybrid&w=740&q=80', alt: 'Evento 4' }
+  ];
 
-  
+  currentIndex = 0;
+  private intervalId?: number;
+  autoplayDelay = 5000;
+  private isBrowser: boolean;
 
-  
+  constructor(@Inject(PLATFORM_ID) platformId: Object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
+
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      this.play();
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.isBrowser) {
+      this.pause();
+    }
+  }
+
+  play(): void {
+    if (!this.isBrowser || this.intervalId) return;
+    this.intervalId = window.setInterval(() => this.nextSlide(), this.autoplayDelay);
+  }
+
+  pause(): void {
+    if (!this.isBrowser) return;
+    if (this.intervalId !== undefined) {
+      clearInterval(this.intervalId);
+      this.intervalId = undefined;
+    }
+  }
+
+  nextSlide(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+  }
+
+  prevSlide(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+  }
+
+  goToSlide(i: number): void {
+    this.currentIndex = i;
+  }
 }
