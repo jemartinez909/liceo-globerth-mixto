6e72e1664116a49682291676e6a21f102cb8887d
jemartinez909 <martinezsanchezesteban15@gmail.com>
2025-09-17 13:16:42 -0500

    Solución de errores dentro del proyecto

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,7 @@
-import {
-  AngularNodeAppEngine,
-  createNodeRequestHandler,
-  isMainModule,
-  writeResponseToNodeResponse,
-} from '@angular/ssr/node';
+import {AngularNodeAppEngine, createNodeRequestHandler, isMainModule, writeResponseToNodeResponse} from '@angular/ssr/node';
 import express from 'express';
 import { join } from 'node:path';
+
 
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
