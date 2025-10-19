3737feba8ad4394fcc9e147b1d18f0e8467ababc
jemartinez909 <martinezsanchezesteban15@gmail.com>
2025-10-18 21:34:29 -0500

    ajustes internos

--- src/app/modules/vida-escolar/pages/main/main.ts
+++ src/app/modules/vida-escolar/pages/main/main.ts
@@ -7,17 +7,7 @@ import { trigger, transition, style, animate } from '@angular/animations';
   standalone: false,
   templateUrl: './main.html',
   styleUrl: './main.sass',
-  animations: [
-    trigger('slideAnimation', [
-      transition(':enter', [
-        style({ opacity: 0, transform: 'translateX(20px)' }),
-        animate('500ms ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
-      ]),
-      transition(':leave', [
-        animate('300ms ease-in', style({ opacity: 0, transform: 'translateX(-20px)' }))
-      ])
-    ])
-  ]
+  
 })
 
 export class Main  {
